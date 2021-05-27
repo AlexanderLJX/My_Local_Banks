0e1540e40121ab52d8bd7f575b0e4d693aa66d5d
Alexander-Lim <ihbcsif2@gmail.com>
2021-05-27 14:54:25 +0800

    Additional Challenge

--- app/src/main/java/com/id20048076/mylocalbanks/MainActivity.java
+++ app/src/main/java/com/id20048076/mylocalbanks/MainActivity.java
@@ -19,6 +19,28 @@ public class MainActivity extends AppCompatActivity {
     TextView tvUOB;
     String bankClicked = "";
 
+
+    String link;
+    String dial;
+    String favourite;
+    String english;
+    String chinese;
+
+    String dbse;
+    String ocbce;
+    String uobe;
+
+    String dbsc;
+    String ocbcc;
+    String uobc;
+
+    String dbsl;
+    String dbsd;
+    String ocbcl;
+    String ocbcd;
+    String uobl;
+    String uobd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,48 +54,87 @@ public class MainActivity extends AppCompatActivity {
         registerForContextMenu(tvOCBC);
         registerForContextMenu(tvUOB);
 
+        link = getString(R.string.link);
+        dial = getString(R.string.dial);
+        favourite = getString(R.string.favourite);
+        english = getString(R.string.english);
+        chinese = getString(R.string.chinese);
+
+        dbse = getString(R.string.dbs);
+        ocbce = getString(R.string.ocbc);
+        uobe = getString(R.string.uob);
+
+        dbsc = getString(R.string.dbsc);
+        ocbcc = getString(R.string.ocbcc);
+        uobc = getString(R.string.uobc);
+
+        dbsl = getString(R.string.dbsl);
+        dbsd = getString(R.string.dbsd);
+        ocbcl = getString(R.string.ocbcl);
+        ocbcd = getString(R.string.ocbcd);
+        uobl = getString(R.string.uobl);
+        uobd = getString(R.string.uobd);
     }
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
         if(v==tvDBS){
-            bankClicked = "DBS";
+            bankClicked = dbse;
         }else if(v==tvOCBC){
-            bankClicked = "OCBC";
+            bankClicked = ocbce;
         }else if(v==tvUOB){
-            bankClicked = "UOB";
+            bankClicked = uobe;
         }
-        menu.add(0,0,0,"Website");
-        menu.add(0,1,1,"Contact Bank");
+        menu.add(0,0,0,link);
+        menu.add(0,1,1,dial);
+        menu.add(0,2,2,favourite);
     }
 
     @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        if(bankClicked.equalsIgnoreCase("DBS")){
+        if(bankClicked.equalsIgnoreCase(dbse)){
             if(id==0){
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.dbs.com.sg"));
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(dbsl));
                 startActivity(intent);
             }else if(id==1){
-                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:18001111111"));
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse(dbsd));
                 startActivity(intentCall);
+            }else if(id==2){
+                int red = getResources().getColor(R.color.red);
+                int black = getResources().getColor(R.color.black);
+                tvDBS.setTextColor(red);
+                tvOCBC.setTextColor(black);
+                tvUOB.setTextColor(black);
             }
-        }else if(bankClicked.equalsIgnoreCase("OCBC")){
+        }else if(bankClicked.equalsIgnoreCase(ocbce)){
             if(id==0){
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ocbc.com"));
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(ocbcl));
                 startActivity(intent);
             }else if(id==1){
-                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:18003633333"));
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse(ocbcd));
                 startActivity(intentCall);
+            }else if(id==2){
+                int red = getResources().getColor(R.color.red);
+                int black = getResources().getColor(R.color.black);
+                tvDBS.setTextColor(black);
+                tvOCBC.setTextColor(red);
+                tvUOB.setTextColor(black);
             }
-        }else if(bankClicked.equalsIgnoreCase("UOB")){
+        }else if(bankClicked.equalsIgnoreCase(uobe)){
             if(id==0){
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.uob.com.sg"));
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uobl));
                 startActivity(intent);
             }else if(id==1){
-                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:18002222121"));
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse(uobd));
                 startActivity(intentCall);
+            }else if(id==2){
+                int red = getResources().getColor(R.color.red);
+                int black = getResources().getColor(R.color.black);
+                tvDBS.setTextColor(black);
+                tvOCBC.setTextColor(black);
+                tvUOB.setTextColor(red);
             }
         }
         return super.onContextItemSelected(item);
@@ -81,8 +142,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(0,0,0,"English");
-        menu.add(0,1,1,"Chinese");
+        menu.add(0,0,0,english);
+        menu.add(0,1,1,chinese);
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -90,13 +151,13 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         if(id==0){
-            tvDBS.setText("DBS");
-            tvOCBC.setText("OCBC");
-            tvUOB.setText("UOB");
+            tvDBS.setText(dbse);
+            tvOCBC.setText(ocbce);
+            tvUOB.setText(uobe);
         }else if(id==1){
-            tvDBS.setText("星展银行");
-            tvOCBC.setText("华侨银行");
-            tvUOB.setText("大华银行");
+            tvDBS.setText(dbsc);
+            tvOCBC.setText(ocbcc);
+            tvUOB.setText(uobc);
         }
         return super.onOptionsItemSelected(item);
     }
